9253d8dd74994153963491e7e6ab367fee67b67f
Julen <j.aguero.fernandez05@gmail.com>
2025-01-15 22:06:18 +0100

    simple database connexion class

--- database/database.php
+++ database/database.php
@@ -2,6 +2,16 @@
 
 include "../config.php";
 
+class Database {
 
+    public function Conexion($Driver, $Host, $Port, $DBName, $User, $Password) 
+    {
+
+        $conn = new PDO($Driver. ":host=" . $Host . ":" . $Port . ";dbname=" . $DBName, $User, $Password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        
+        return $conn;
+    }
+}
 
 ?>
