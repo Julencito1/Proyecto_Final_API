03af084e50258a87e7a513bec340d6d93773560b
Julen <j.aguero.fernandez05@gmail.com>
2025-01-17 17:43:38 +0100

    updated DBName const

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 define("DRIVER", "mysql");
 define("HOST", "localhost");
 define("PORT", "3307");
-define("DBNAME", ""); // poner nombre db
+define("DBNAME", "proyecto_final"); 
 define("USER", "root");
 define("PASSWORD", "");
 
