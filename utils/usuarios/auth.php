67c5d163f85013b30c3342d6dbe700510085d86d
Julen <j.aguero.fernandez05@gmail.com>
2025-01-23 22:17:06 +0100

    extra functions for users class

---
+++ utils/usuarios/auth.php
@@ -0,0 +1,42 @@
+<?php 
+
+function ExisteUsuario($con, $email): bool
+{
+    $q = "SELECT COUNT(*) AS existe FROM usuarios WHERE email = :email";
+
+    $consulta = $con->prepare($q);
+
+    $consulta->execute(["email" => $email]);
+
+    $respuesta = $consulta->fetch(PDO::FETCH_ASSOC);
+
+    return $respuesta["existe"] === 0;
+}
+
+function ObtenerPasswordHash($con, $email): array
+{
+    $q = "SELECT password FROM usuarios WHERE email = :email";
+
+    $consulta = $con->prepare($q);
+
+    $estado = $consulta->execute(["email" => $email]);
+
+    $respuesta = $consulta->fetch(PDO::FETCH_ASSOC);
+
+    if ($estado) {
+
+        return [$respuesta["password"], "S"];
+
+    } else {
+        
+        return ["", "N"];
+    }
+
+
+    
+}
+
+
+
+
+?>
