3ebfa51b6eab44a50b6d9b04e309bce99eb0e3de
Julen <j.aguero.fernandez05@gmail.com>
2025-05-09 16:00:20 +0200

    suscribers models file

---
+++ models/suscripciones/Modelos.php
@@ -0,0 +1,43 @@
+<?php
+
+
+namespace Models\Suscripciones;
+
+class Modelos
+{
+
+    private $suscripciones;
+
+    public function __construct($suscripciones)
+    {
+        $this->suscripciones = $suscripciones;
+    }
+
+    public static function SidebarSuscripcionesArray($nombre, $avatar, $nombre_canal)
+    {
+        return [
+           
+                "usuario" => [
+                    "nombre" => $nombre,
+                    "media" => [
+                        "avatar" => $avatar,
+                    ],
+                    "canal" => [
+                        "nombre_canal" => $nombre_canal,
+                    ],
+                ],
+            
+        ];
+    }
+
+    public static function SidebarSuscripciones($array, $mostrar)
+    {
+        return [
+            "suscripciones" => [
+                "listar" => $array,
+                "mostrar" => $mostrar,
+            ],
+        ];
+    }
+}
+
