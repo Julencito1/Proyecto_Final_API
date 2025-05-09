8570c94204961eb2538f9f770150115f4a7d5f3c
Julen <j.aguero.fernandez05@gmail.com>
2025-05-09 16:07:09 +0200

    generate identifiers, etc for videos file

--- utils/videos/generar.php
+++ utils/videos/generar.php
@@ -4,37 +4,37 @@ namespace Utils\Videos;
 use Utils\Caracteres\Caracteres;
 use PDO;
 
-class Generar extends Videos
+class Generar
 {
     
-    protected function GenerarIdentificador()
-    {
+    // protected function GenerarIdentificador()
+    // {
 
-        $estado = true;
+    //     $estado = true;
 
-        while ($estado) {
-            $identificador = "";
+    //     while ($estado) {
+    //         $identificador = "";
 
-            for ($x = 0; $x < 20; $x++) {
+    //         for ($x = 0; $x < 20; $x++) {
 
-                $identificador .= Caracteres::$letras_numeros[rand(0, count(Caracteres::$letras_numeros) - 1)];
-            }
+    //             $identificador .= Caracteres::$letras_numeros[rand(0, count(Caracteres::$letras_numeros) - 1)];
+    //         }
 
-            $consultaExiste = $this->con->prepare("SELECT COUNT(*) AS existe FROM " . self::$tabla . " WHERE identificador = :identificador");
+    //         $consultaExiste = $this->con->prepare("SELECT COUNT(*) AS existe FROM " . self::$tabla . " WHERE identificador = :identificador");
 
-            $consultaExiste->execute(["identificador" => $identificador]);
+    //         $consultaExiste->execute(["identificador" => $identificador]);
 
-            $respuesta = $consultaExiste->fetch(PDO::FETCH_ASSOC);
+    //         $respuesta = $consultaExiste->fetch(PDO::FETCH_ASSOC);
 
-            if ($respuesta["existe"] === 0) {
-                return $identificador;
-            }
+    //         if ($respuesta["existe"] === 0) {
+    //             return $identificador;
+    //         }
 
-        }
+    //     }
 
-        return "";
+    //     return "";
 
-    }
+    // }
 
 }
 
