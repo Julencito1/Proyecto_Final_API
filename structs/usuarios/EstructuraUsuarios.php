46b4d8383b129d799b93083964c4d298c6a6539b
Julen <j.aguero.fernandez05@gmail.com>
2025-05-09 16:03:10 +0200

    Users structs

---
+++ structs/usuarios/EstructuraUsuarios.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace Structs\Usuarios;
+
+
+
+abstract class EstructuraUsuarios
+{
+    abstract public function Registro();
+    abstract public function Login();
+    abstract public function DatosUsuario();
+}
+
+    
+
+?>
