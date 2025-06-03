ef5c376f218077699aa461352a804db83cd1acb5
Julen <j.aguero.fernandez05@gmail.com>
2025-06-03 20:40:09 +0200

    use user id instead of user identifier

--- controllers/notificaciones/Notificaciones.php
+++ controllers/notificaciones/Notificaciones.php
@@ -6,6 +6,7 @@ use Models\Notificaciones\Modelos;
 use PDO;
 use Structs\Notificaciones\EstructuraNotificaciones;
 use Utils\Auth\Auth;
+use Utils\Usuarios\Obtener;
 
 class Notificaciones extends EstructuraNotificaciones
 {
@@ -68,13 +69,14 @@ class Notificaciones extends EstructuraNotificaciones
 
         $limit = $datos["limit"];
         $offset = $datos["offset"];
+        $usuarioID = Obtener::Id($identificador, $this->con);
 
         $q = "
-           SELECT n.enlace, n.fecha, v.titulo, v.miniatura, c.nombre_canal, u.avatar FROM notificaciones n LEFT JOIN canales c ON c.id = n.canal_id LEFT JOIN videos v ON v.id = n.video_id LEFT JOIN usuarios u ON u.id = c.usuario_id WHERE u.identificador = ? AND n.fecha >= DATE_SUB(NOW(), INTERVAL 1 WEEK) ORDER BY n.fecha DESC LIMIT " . $limit . " OFFSET ". $offset . ";
+           SELECT n.enlace, n.fecha, v.titulo, v.miniatura, c.nombre_canal, u.avatar FROM notificaciones n LEFT JOIN canales c ON c.id = n.canal_id LEFT JOIN videos v ON v.id = n.video_id LEFT JOIN usuarios u ON u.id = c.usuario_id WHERE n.usuario_id = ? AND n.fecha >= DATE_SUB(NOW(), INTERVAL 1 WEEK) ORDER BY n.fecha DESC LIMIT " . $limit . " OFFSET ". $offset . ";
         ";
 
         $obtenerNotificaciones = $this->con->prepare($q);
-        $obtenerNotificaciones->bindValue(1, $identificador);
+        $obtenerNotificaciones->bindValue(1, $usuarioID);
         
         $estado = $obtenerNotificaciones->execute();
         $respuesta = $obtenerNotificaciones->fetchAll(PDO::FETCH_ASSOC);
