bc9590c8e33200675c555fefe8e934c5e463d712
Julen Aguero <49107063+Julencito1@users.noreply.github.com>
2025-06-12 08:58:35 +0200

    semicolon

--- utils/auth/Auth.php
+++ utils/auth/Auth.php
@@ -17,10 +17,10 @@ class Auth {
                 $decode = JWT::decode($token, new Key("18dddd6d-bef4-44fe-9b92-f67030332b3f", "HS256"));
 
                 return $decode->seed;
-                
+
             } catch(Exception $e)
             {
-                return ""
+                return "";
             }
         }
 
